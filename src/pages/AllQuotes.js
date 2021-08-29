72ea5fdc2d09d2d47e4d0f789662e556172c7b50
Ronald Prajval <ronaldprajval7@gmail.com>
2021-08-29 12:11:37 +0530

    adding dummy data and more content

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,20 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Mugabe",
+    text: "If you're ugly, you're ugly.",
+  },
+  {
+    id: "q2",
+    author: "Slim shady",
+    text: "Palms are sweaty, knees weak arms are heavy.",
+  },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes PAge</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,5 +1,11 @@
+import QuoteForm from "../components/quotes/QuoteForm";
+
 const NewQuote = () => {
-  return <h1>New Quote PAge</h1>;
+  const addQuoteHandler = (quoteData) => {
+    console.log(quoteData);
+  };
+
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuote;
